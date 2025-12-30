306591356a34da32673c8dd64550c614283e6573
Akio <83323341+Hunter-GG@users.noreply.github.com>
2025-12-30 23:44:07 +0200

    Delete SQL/queries.sql

--- SQL/queries.sql
+++
@@ -1,9 +0,0 @@
--- Query 1: All reservations for a customer
-SELECT *
-FROM Reservation
-WHERE customer_id = 1;
-
--- Query 2: Customer names with reservation dates
-SELECT Customer.name, Reservation.reservation_date
-FROM Customer
-JOIN Reservation ON Customer.customer_id = Reservation.customer_id;
